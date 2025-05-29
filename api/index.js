eb1a0a55479a910e5ca85cbd84dff78b6390045e
Eshwar Sai G A <eashwarsaiga@gmail.com>
2025-05-29 20:49:38 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -22,4 +22,10 @@ app.get('/', (req, res)=>{
     return res.send("API WORKING")
 })
 
+app.use(express.static(path.join(__dirname, 'client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+
 app.listen(PORT, ()=>console.log("Server running on port " + PORT))
